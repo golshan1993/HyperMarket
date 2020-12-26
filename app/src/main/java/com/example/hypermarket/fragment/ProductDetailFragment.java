257ac2c97b4c95e4fb6a110710228e7a0c43c5b1
Golshan Piroozzadeh <47850735+golshan1993@users.noreply.github.com>
2020-12-26 20:51:49 +0330

    Setting progress bar for the product detail pages. Setting javascript enabled. Using web chrome client for setting title for application.

--- app/src/main/java/com/example/hypermarket/fragment/ProductDetailFragment.java
+++ app/src/main/java/com/example/hypermarket/fragment/ProductDetailFragment.java
@@ -2,12 +2,17 @@ package com.example.hypermarket.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.example.hypermarket.R;
 
@@ -16,6 +21,7 @@ public class ProductDetailFragment extends Fragment {
     public static final String USER_WEB_PAGE_ADDRESS = "userWebPageAddress";
     private WebView mWebView;
     private String webPageSelected;
+    private ProgressBar mProgressBar;
     public ProductDetailFragment() {
         // Required empty public constructor
     }
@@ -42,7 +48,32 @@ public class ProductDetailFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_product_detail, container, false);
         mWebView = view.findViewById(R.id.web_view_product);
+        mProgressBar = view.findViewById(R.id.progress_bar);
+        mProgressBar.setMax(100);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient(){
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                view.loadUrl(url);
+                return(false);
+            }
+        });
         mWebView.loadUrl(webPageSelected);
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            public void onProgressChanged(WebView view, int newProgress){
+                super.onProgressChanged(view, newProgress);
+                if (newProgress == 100){
+                    mProgressBar.setVisibility(View.GONE);
+                }
+                else{
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+            }
+            public void onReceivedTitle(WebView view, String title){
+                AppCompatActivity activity = (AppCompatActivity) getActivity();
+                activity.getSupportActionBar().setSubtitle(title);
+            }
+        });
         return view;
     }
 }
